e1055abda13b0ab510da4833bc7fe1a235984f83
svats07 <100149130+svats07@users.noreply.github.com>
2026-01-15 16:55:31 +0530

    MObile Responsice

--- src/components/Associates.js
+++ src/components/Associates.js
@@ -19,12 +19,12 @@ export default function Associates(){
       <h2>Our Associates</h2>
       <p className="section-subtitle">Meet Our Expert Legal Team</p>
       <div className="associates-grid container">
-        <Associate name="Dr. N C Sharma" title="Managing Partner" img="mosaji.png" bio="25 years specializing in criminal law." />
-        <Associate name="Ajay Sharma" title="Partner" img="tinu.png" bio="Civil litigation expert with 18 years experience. Known for strategic courtroom excellence." />
-        <Associate name="Anand Lochave" title="Senior Associate" img="Anand.jpeg" bio="Criminal Law specialist with extensive experience in commercial transactions." />
-        <Associate name="OP Sharma" title="Senior Associate" img="op.jpeg" bio="Family law attorney dedicated to compassionate representation in sensitive matters." />
-        <Associate name="Pankaj Bhardwaj" title="Senior Associate" img="pankaj.jpeg" bio="Family law attorney dedicated to compassionate representation in sensitive matters." />
-        <Associate name="Mukul Vats" title="Associate" img="Mukul.jpeg" bio="Criminal law attorney dedicated to compassionate representation in sensitive matters." />
+        <Associate name="Dr. N C Sharma" title="Managing Partner" img="mosaji.jpg" bio="25 years specializing in criminal law." />
+        <Associate name="Ajay Sharma" title="Partner" img="tinu.jpg" bio="Civil litigation expert with 18 years experience. Known for strategic courtroom excellence." />
+        <Associate name="Anand Lochave" title="Senior Associate" img="Anand.jpg" bio="Criminal Law specialist with extensive experience in commercial transactions." />
+        <Associate name="Pankaj Bhardwaj" title="Senior Associate" img="pankaj.jpg" bio="Family law attorney dedicated to compassionate representation in sensitive matters." />
+        <Associate name="Anshul Vats" title="Associate" img="Anshul.jpg" bio="Family law attorney dedicated to compassionate representation in sensitive matters." />
+        <Associate name="Mukul Vats" title="Associate" img="Mukul.jpg" bio="Criminal law attorney dedicated to compassionate representation in sensitive matters." />
       </div>
     </section>
   );
